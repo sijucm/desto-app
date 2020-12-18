c2e78f31b1fe12d6066211ec5459139a5e32d536
sijucm <siju.mathew@nl.abnamro.com>
2020-12-18 12:29:54 +0100

    Concise view of the matches for easy screenshot

--- src/components/view/MatchResultView.vue
+++ src/components/view/MatchResultView.vue
@@ -6,20 +6,28 @@
         <div class="col">
           <ul class=" score-a list-group match-card list-group-flush mt-2">
             <li class="list-group-item match-card p-0 d-flex justify-content-center"> {{ teams[0] }}
-                            <result-badge-view class="ml-2" :shown="getClassForTeamZero"></result-badge-view>
+              <result-badge-view class="ml-2" :shown="getClassForTeamZero"></result-badge-view>
             </li>
-            <li class="list-group-item match-card p-0 d-flex justify-content-center goals">
-              {{ results.hasOwnProperty(teams[0]) && results[teams[0]] !=null ?results[teams[0]]:"..." }}
+            <li v-if="showResults"
+                class="list-group-item match-card p-0 d-flex justify-content-center goals">
+              {{
+                results.hasOwnProperty(teams[0]) && results[teams[0]] != null ? results[teams[0]]
+                    : "..."
+              }}
             </li>
           </ul>
         </div>
         <div class="col ">
           <ul class=" score-a list-group list-group-flush mt-2 ">
             <li class="list-group-item match-card p-0 d-flex justify-content-center"> {{ teams[1] }}
-                            <result-badge-view class="ml-2" :shown="getClassForTeamOne"></result-badge-view>
+              <result-badge-view class="ml-2" :shown="getClassForTeamOne"></result-badge-view>
             </li>
-            <li class="list-group-item match-card p-0 d-flex justify-content-center goals">
-              {{ results.hasOwnProperty(teams[1])  && results[teams[1]] !=null ?results[teams[1]]:"..." }}
+            <li v-if="showResults"
+                class="list-group-item match-card p-0 d-flex justify-content-center goals">
+              {{
+                results.hasOwnProperty(teams[1]) && results[teams[1]] != null ? results[teams[1]]
+                    : "..."
+              }}
             </li>
           </ul>
         </div>
@@ -33,6 +41,7 @@
 <script>
 
 import ResultBadgeView from "@/components/view/ResultBadgeView";
+
 export default {
   name: "MatchResultView",
   components: {ResultBadgeView},
@@ -48,8 +57,8 @@ export default {
   },
   computed: {
     getClassForTeamZero() {
-      if(!( Object.prototype.hasOwnProperty.call(this.results, this.teams[0])
-          && Object.prototype.hasOwnProperty.call(this.results, this.teams[1]))){
+      if (!(Object.prototype.hasOwnProperty.call(this.results, this.teams[0])
+          && Object.prototype.hasOwnProperty.call(this.results, this.teams[1]))) {
         return 0;
       }
 
@@ -62,8 +71,8 @@ export default {
       }
     },
     getClassForTeamOne() {
-      if(!( Object.prototype.hasOwnProperty.call(this.results, this.teams[0])
-          && Object.prototype.hasOwnProperty.call(this.results, this.teams[1]))){
+      if (!(Object.prototype.hasOwnProperty.call(this.results, this.teams[0])
+          && Object.prototype.hasOwnProperty.call(this.results, this.teams[1]))) {
         return 0;
       }
       if (this.results[this.teams[1]] > this.results[this.teams[0]]) {
@@ -75,6 +84,12 @@ export default {
       }
 
     },
+
+    showResults() {
+      return Object.prototype.hasOwnProperty.call(this.results, this.teams[1])
+      && Object.prototype.hasOwnProperty.call(this.results, this.teams[0])
+
+    }
 
   }
 }
